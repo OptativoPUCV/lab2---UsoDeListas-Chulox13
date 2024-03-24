618dbd9b47dc27edc6e5afd252778fba89cb5cbd
Your Name <you@example.com>
2024-03-24 01:08:03 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -135,7 +135,7 @@ int parentesisBalanceados(char *cadena)
 {
   for (; *cadena != '\0'; cadena++) {
       if (*cadena == '(' || *cadena == '[' || *cadena == '{') {
-          push(pila, *cadena);
+          push(pila, cadena);
       } else if (*cadena == ')' || *cadena == ']' || *cadena == '}') {
           if (top(pila) != NULL) {
               return 0; // Más paréntesis cerrados que abiertos
